1b777c4dc684effaaf9ad7ee50b7d84ffd47fcfd
Ács Dávid <acsdavid97@gmail.com>
2018-04-26 11:21:24 +0300

    labeling of thresholded objects, and filtering by area

--- OpenCVApplication-VS2017_OCV340_basic/OpenCVApplication.cpp
+++ OpenCVApplication-VS2017_OCV340_basic/OpenCVApplication.cpp
@@ -3,7 +3,57 @@
 
 #include "stdafx.h"
 #include "common.h"
+#include <unordered_set>
 
+/*
+ * Sets the labels to zero (set them as background) if the label is contained in labels_to_remove
+ */
+void removeLabels(Mat labeled_image, std::unordered_set<int> labels_to_remove)
+{
+	for(int rowIndex = 0; rowIndex < labeled_image.rows; rowIndex++)
+	{
+		for(int colIndex = 0; colIndex < labeled_image.cols; colIndex++)
+		{
+			int label = labeled_image.at<int>(rowIndex, colIndex);
+			const bool remove_label = labels_to_remove.find(label) != labels_to_remove.end();
+			if (remove_label)
+			{
+				labeled_image.at<int>(rowIndex, colIndex) = 0;
+			}
+		}
+	}
+
+}
+
+Mat filterSmallAndLargeObjects(Mat binary_image)
+{
+	Mat labeled_image;
+	Mat stats;
+	Mat centroids;
+	int labelCount = connectedComponentsWithStats(binary_image, labeled_image, stats, centroids);
+
+	// a road sign should not occupy more than 10 % of the image area
+	const int total_image_area = binary_image.cols * binary_image.rows;
+	const int max_area = total_image_area / 10;
+	// a road sign should be at least 0.5% of pixels to be recognizable
+	const int min_area = (total_image_area) / 200;
+	std::unordered_set<int> labels_to_remove;
+
+	for(int labelIndex = 0; labelIndex < labelCount; labelIndex++)
+	{
+		int area = stats.at<int>(labelIndex, CC_STAT_AREA);
+		if (area < min_area || max_area < area)
+		{
+			labels_to_remove.insert(labelIndex);
+		}
+	}
+
+	removeLabels(labeled_image, labels_to_remove);
+
+	Mat converted_labeled;
+	labeled_image.convertTo(converted_labeled, CV_8UC1);
+	return converted_labeled;
+}
 
 void convertToBinaryRed() 
 {
@@ -36,19 +86,27 @@ void convertToBinaryRed()
 		const int dialtion_size = 2;
 		const int element_size = 2 * dialtion_size + 1;
 
-		Mat element = getStructuringElement(MORPH_CROSS, Size(element_size, element_size));
-		/*
+		Mat element = getStructuringElement(MORPH_RECT, Size(element_size, element_size));
+
 		erode(maskR1|maskR2, maskR_eroded, element);
 		dilate(maskR_eroded, maskR_opened, element);
-		*/
+
+		Mat filtered = filterSmallAndLargeObjects(maskR_opened);
+		
+		Mat filtered_equalized;
+		equalizeHist(filtered, filtered_equalized);
+
+		Mat labeled;
+		applyColorMap(filtered_equalized, labeled, COLORMAP_JET);
 
 		imshow("input image", src);
-		imshow("only red", maskR1 | maskR2);
-
-
+		imshow("only red", maskR_opened);
+		imshow("labeled", labeled);
+		waitKey();
 	}
 	
 }
+
 
 void convertToBinaryYellow() {
 	char fname[MAX_PATH];

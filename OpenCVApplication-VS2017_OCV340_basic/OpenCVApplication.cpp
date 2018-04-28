b604eba30bf3e504986294cdba29ab336fd0d5d7
Ács Dávid <acsdavid97@gmail.com>
2018-04-28 21:33:19 +0300

    GUI road sign detection -- can change parameters from GUI, and will label the detected images

--- OpenCVApplication-VS2017_OCV340_basic/OpenCVApplication.cpp
+++ OpenCVApplication-VS2017_OCV340_basic/OpenCVApplication.cpp
@@ -22,21 +22,16 @@ void removeLabels(Mat labeled_image, std::unordered_set<int> labels_to_remove)
 			}
 		}
 	}
-
 }
 
-Mat filterSmallAndLargeObjects(Mat binary_image)
+Mat filterSmallAndLargeObjects(Mat binary_image, int min_area, int max_area)
 {
 	Mat labeled_image;
 	Mat stats;
 	Mat centroids;
 	int labelCount = connectedComponentsWithStats(binary_image, labeled_image, stats, centroids);
 
-	// a road sign should not occupy more than 10 % of the image area
 	const int total_image_area = binary_image.cols * binary_image.rows;
-	const int max_area = total_image_area / 10;
-	// a road sign should be at least 0.5% of pixels to be recognizable
-	const int min_area = (total_image_area) / 200;
 	std::unordered_set<int> labels_to_remove;
 
 	for(int labelIndex = 0; labelIndex < labelCount; labelIndex++)
@@ -57,7 +52,6 @@ Mat filterSmallAndLargeObjects(Mat binary_image)
 
 void convertToBinaryRed() 
 {
-
 	char fname[MAX_PATH];
 	while (openFileDlg(fname))
 	{
@@ -91,7 +85,10 @@ void convertToBinaryRed()
 		erode(maskR1|maskR2, maskR_eroded, element);
 		dilate(maskR_eroded, maskR_opened, element);
 
-		Mat filtered = filterSmallAndLargeObjects(maskR_opened);
+		int total_area = src.rows * src.cols;
+		int max_area = total_area / 10;
+		int min_area = total_area / 200;
+		Mat filtered = filterSmallAndLargeObjects(maskR_opened, min_area, max_area);
 		
 		Mat filtered_equalized;
 		equalizeHist(filtered, filtered_equalized);
@@ -181,11 +178,111 @@ void convertToBinaryBlue() {
 		*/
 		imshow("input image", src);
 		imshow("only blue", maskB);
-
-
 	}
 }
 
+int hue_start = 160;
+int hue_offset = 40;
+int min_value = 100;
+int min_saturation = 100;
+int opening_size = 2;
+int min_area_perc = 5;
+int max_area_perc = 100;
+
+Mat convertToBinaryColor(Mat src, int hue_start, int hue_offset, int min_saturation, int min_value)
+{
+	
+	Mat hsvImg;
+	cv::cvtColor(src, hsvImg, CV_BGR2HSV);
+
+	//define lower and upper bounds for meaningful colors in hsv
+
+	Mat mask;
+	cv::Scalar lower(hue_start, min_saturation, min_value);
+	if (hue_start + hue_offset < 180)
+	{
+		cv::Scalar upper(hue_start + hue_offset, 255, 255);
+		cv::inRange(hsvImg, lower, upper, mask);
+		return mask;
+	}
+
+	int remaining_hue = (hue_start + hue_offset) % 180;
+
+	cv::Scalar  middle1(179, 255, 255);
+	cv::Scalar  middle2(0, min_saturation, min_value);
+	cv::Scalar  upper(remaining_hue, 255, 255);
+
+	Mat mask_extended;
+
+	//filter out less meaningful colors
+	cv::inRange(hsvImg, lower, middle1, mask);
+	cv::inRange(hsvImg, middle2, upper, mask_extended);
+	return mask | mask_extended;
+}
+
+void detectRoadSignCallback(int event, int x, int y, int flags, void* data)
+{
+	if (event != CV_EVENT_LBUTTONDOWN)
+	{
+		return;
+	}
+	Mat src = *(Mat*)data;
+
+	//HSV image
+	Mat mask = convertToBinaryColor(src, hue_start, hue_offset, min_saturation, min_value);
+
+	//opening
+	const int dialtion_size = opening_size;
+	const int element_size = 2 * dialtion_size + 1;
+
+	Mat element = getStructuringElement(MORPH_RECT, Size(element_size, element_size));
+
+	Mat mask_eroded,mask_opened;
+	erode(mask, mask_eroded, element);
+	dilate(mask_eroded, mask_opened, element);
+
+	int total_area = src.rows * src.cols;
+	int min_area = (total_area * min_area_perc) / 1000;
+	int max_area = (total_area * max_area_perc) / 1000;
+	Mat filtered = filterSmallAndLargeObjects(mask_opened, min_area, max_area);
+	
+	Mat filtered_equalized;
+	equalizeHist(filtered, filtered_equalized);
+
+	Mat labeled;
+	applyColorMap(filtered_equalized, labeled, COLORMAP_JET);
+
+	imshow("input image", src);
+	imshow("only red", mask_opened);
+	imshow("labeled", labeled);
+}
+
+void guiDetectRoadSign()
+{
+	
+	char fname[MAX_PATH];
+	while (openFileDlg(fname))
+	{
+		Mat src = imread(fname);
+		const std::string trackbar_window_name = "trackbars";
+		namedWindow(trackbar_window_name);
+		createTrackbar("hue start", trackbar_window_name, &hue_start, 179);
+		createTrackbar("hue offset", trackbar_window_name, &hue_offset, 179);
+		createTrackbar("min value", trackbar_window_name, &min_value, 255);
+		createTrackbar("min staturation", trackbar_window_name, &min_saturation, 255);
+		createTrackbar("opening size", trackbar_window_name, &opening_size, 20);
+		createTrackbar("min area percentage", trackbar_window_name, &min_area_perc, 100);
+		createTrackbar("max area percentage", trackbar_window_name, &max_area_perc, 500);
+
+		namedWindow("buttonwindow");
+		setMouseCallback("buttonwindow", detectRoadSignCallback, &src);
+		Mat button = Mat(100, 100, CV_8UC1);
+		imshow("buttonwindow", button);
+
+		detectRoadSignCallback(0, 0, 0, 0, &src);
+		waitKey();
+	}
+}
 
 int main()
 {
@@ -198,6 +295,7 @@ int main()
 		printf(" 1 - Convert image to binary based on red\n");
 		printf(" 2 - Convert image to binary based on blue\n");
 		printf(" 3 - Convert image to binary based on yellow\n");
+		printf(" 4 - GUI road sign detection\n");
 		printf(" 0 - Exit\n\n");
 		printf("Option: ");
 		scanf("%d",&op);
@@ -211,6 +309,9 @@ int main()
 				break;
 			case 3:
 				convertToBinaryYellow();
+				break;
+			case 4:
+				guiDetectRoadSign();
 				break;
 
 				
